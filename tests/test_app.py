4353166bef6203f49cd94d954487de23f205342e
hunain raza <i221614@nu.edu.pk>
2025-12-30 11:15:26 +0500

    Update test assertion for home route response

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,11 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app.app import app
 
 def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"SSD Final Flask App" in response.data
-
+    assert b"Hello from Flask CI/CD Pipeline!" in response.data
